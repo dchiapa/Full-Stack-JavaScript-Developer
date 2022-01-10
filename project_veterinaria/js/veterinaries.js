ea5f5876075dd6b5c3d77920fa1fa5f3098f32b7
dchiapa <dchiapa@est.frba.utn.edu.ar>
2022-01-10 16:57:19 -0300

    add veterinarios html and js

---
+++ project_veterinaria/js/veterinaries.js
@@ -0,0 +1,82 @@
+const veterinariesList = document.getElementById("veterinariesList");
+const formVeterinary = document.getElementById("formVeterinary");
+const formVeterinaryIndex = document.getElementById("veterinaryIndex");
+const formVeterinaryFirstName = document.getElementById("veterinaryFirstName");
+const formVeterinaryLastName = document.getElementById("veterinaryLastName");
+const formVeterinaryCountry = document.getElementById("veterinaryCountry");
+const btnCloseVeterinary = document.getElementById("btnCloseVeterinary");
+const btnSaveVeterinary = document.getElementById("btnSaveVeterinary");
+
+let veterinaries = [
+  { firstName: "Diego", lastName: "Chiapa", country: "Argentina" },
+  { firstName: "Juan", lastName: "Castillejo", country: "Uruguay" },
+];
+
+const renderVeterinaries = () => {
+  const htmlVeterinaries = veterinaries
+    .map(
+      (veterinary, index) => `
+    <tr>
+      <th scope="row">${index}</th>
+      <td>${veterinary.firstName}</td>
+      <td>${veterinary.lastName}</td>
+      <td>${veterinary.country}</td>
+      <td>
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button type="button" class="btn btn-info btnEditVeterinary" data-index=${index} data-toggle="modal"
+          data-target="#modalVeterinary">
+            <i class="fas fa-edit"></i>
+          </button>
+          <button type="button" class="btn btn-danger btnDeleteVeterinary">
+            <i class="fas fa-trash-alt"></i>
+          </button>
+        </div>
+      </td>
+    </tr>`
+    )
+    .join("");
+  veterinariesList.innerHTML = htmlVeterinaries;
+  Array.from(document.getElementsByClassName("btnEditVeterinary")).forEach(
+    (btn, index) => btn.addEventListener("click", openEditVeterinary(index))
+  );
+  Array.from(document.getElementsByClassName("btnDeleteVeterinary")).forEach(
+    (btn, index) => btn.addEventListener("click", deleteVeterinary(index))
+  );
+};
+const openEditVeterinary = (index) => {
+  return () => {
+    formVeterinaryIndex.value = index;
+    formVeterinaryFirstName.value = veterinaries[index].firstName;
+    formVeterinaryLastName.value = veterinaries[index].lastName;
+    formVeterinaryCountry.value = veterinaries[index].country;
+  };
+};
+const submitData = () => {
+  const veterinaryData = {
+    firstName: formVeterinaryFirstName.value,
+    lastName: formVeterinaryLastName.value,
+    country: formVeterinaryCountry.value,
+  };
+  if (formVeterinaryIndex.value !== "") {
+    veterinaries[formVeterinaryIndex.value] = veterinaryData;
+  } else {
+    veterinaries.push(veterinaryData);
+  }
+  closeVeterinary();
+  renderVeterinaries();
+};
+const deleteVeterinary = (index) => {
+  return () => {
+    veterinaries.splice(index, 1);
+    renderVeterinaries();
+  };
+};
+const closeVeterinary = () => {
+  formVeterinaryIndex.value = "";
+  formVeterinary.reset();
+};
+
+formVeterinary.addEventListener("submit", (e) => e.preventDefault());
+btnSaveVeterinary.addEventListener("click", () => submitData());
+btnCloseVeterinary.addEventListener("click", () => closeVeterinary());
+renderVeterinaries();
