8d8af18f2437574b7890e091a69029c53494c5ad
dchiapa <dchiapa@est.frba.utn.edu.ar>
2022-01-14 17:57:30 -0300

    revert port of backend app to 5000

--- veterinaria/backend/index.js
+++ veterinaria/backend/index.js
@@ -3,6 +3,6 @@ const requestHandler = require("./requestHandler");
 
 const server = http.createServer(requestHandler);
 
-server.listen(6000, () => {
-  console.log("Server running on localhost:6000");
+server.listen(5000, () => {
+  console.log("Server running on localhost:5000");
 });
