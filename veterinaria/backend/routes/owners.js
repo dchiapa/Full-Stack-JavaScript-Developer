f379d5c0a349192d562c7e54fb5a3f451bf244fa
dchiapa <dchiapa@est.frba.utn.edu.ar>
2022-01-14 16:38:20 -0300

    add backend files to veterinaria folder and organize all files in folders

---
+++ veterinaria/backend/routes/owners.js
@@ -0,0 +1,52 @@
+module.exports = (owners) => {
+  return {
+    post: (data, callback) => {
+      let newOwner = data.payload;
+      owners = [...owners, newOwner];
+      callback(201, newOwner);
+    },
+    get: (data, callback) => {
+      if (data.index) {
+        if (owners[data.index]) {
+          return callback(200, owners[data.index]);
+        }
+        return callback(404, {
+          message: `No existe un dueño con el indice ${data.index}`,
+        });
+      } else {
+        if (owners.length > 0) {
+          return callback(200, owners);
+        }
+        return callback(404, { message: "No hay dueños registrados" });
+      }
+    },
+    put: (data, callback) => {
+      if (data.index !== null) {
+        if (owners[data.index]) {
+          owners[data.index] = data.payload;
+          return callback(200, owners[data.index]);
+        }
+        return callback(404, {
+          message: `No existe un dueño con el indice ${data.index}`,
+        });
+      } else {
+        return callback(400, { message: `No envió ningún indice` });
+      }
+    },
+    delete: (data, callback) => {
+      if (data.index !== null) {
+        if (owners[data.index]) {
+          owners = owners.filter((_pet, index) => index != data.index);
+          return callback(204, {
+            message: `Dueño con indice ${data.index} eliminado`,
+          });
+        }
+        return callback(404, {
+          message: `No existe un dueño con el indice ${data.index}`,
+        });
+      } else {
+        return callback(400, { message: `No envió ningún indice` });
+      }
+    },
+  };
+};
---
+++ veterinaria/backend/routes/queries.js
@@ -0,0 +1,56 @@
+module.exports = (queries) => {
+  return {
+    post: (data, callback) => {
+      let newQuery = data.payload;
+      newQuery.createdDate = new Date();
+      newQuery.edtiDate = null;
+      queries = [...queries, newQuery];
+      callback(201, data.payload);
+    },
+    get: (data, callback) => {
+      if (data.index) {
+        if (queries[data.index]) {
+          return callback(200, queries[data.index]);
+        }
+        return callback(404, {
+          message: `No existe una consulta con el indice ${data.index}`,
+        });
+      } else {
+        if (queries.length > 0) {
+          return callback(200, queries);
+        }
+        return callback(404, { message: "No hay consultas registradas" });
+      }
+    },
+    put: (data, callback) => {
+      if (data.index !== null) {
+        if (queries[data.index]) {
+          const { createdDate } = queries[data.index];
+          let query = { ...data.payload, createdDate, editDate: new Date() };
+          queries[data.index] = query;
+          return callback(200, queries[data.index]);
+        }
+        return callback(404, {
+          message: `No existe una consulta con el indice ${data.index}`,
+        });
+      } else {
+        return callback(400, { message: `No envió ningún indice` });
+      }
+    },
+    delete: (data, callback) => {
+      if (data.index !== null) {
+        if (queries[data.index]) {
+          queries = queries.filter((_pet, index) => index != data.index);
+          return callback(204, {
+            message: `Consulta con indice ${data.index} eliminada`,
+          });
+        }
+        return callback(404, {
+          message: `No existe una consulta con el indice ${data.index}`,
+        });
+      } else {
+        return callback(400, { message: `No envió ningún indice` });
+      }
+    },
+  };
+};
