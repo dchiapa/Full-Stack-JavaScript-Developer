fb84a2be35fb84980ab40ad89b8f8954ec156909
dchiapa <dchiapa@est.frba.utn.edu.ar>
2022-01-15 22:53:33 -0300

    add renderQueries() to queries front

---
+++ veterinaria/frontend/js/queries.js
@@ -0,0 +1,41 @@
+const queriesList = document.getElementById("queriesList");
+const apiUrl = "http://127.0.0.1:5000/queries";
+
+let queries = [];
+
+const renderQueries = async () => {
+  try {
+    const response = await fetch(apiUrl);
+    const queries = await response.json();
+    if (Array.isArray(queries)) {
+      const htmlQueries = queries
+        .map(
+          (query, index) => `
+          <tr>
+            <th scope="row">${index}</th>
+            <td>${query.pet}</td>
+            <td>${query.veterinary}</td>
+            <td>${query.addedDate}</td>
+            <td>${query.editDate}</td>
+            <td>
+              <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-info btnEditVeterinary" data-index=${index} data-toggle="modal"
+                data-target="#modalVeterinary">
+                  <i class="fas fa-edit"></i>
+                </button>
+              </div>
+            </td>
+          </tr>`
+        )
+        .join("");
+      queriesList.innerHTML = htmlQueries;
+      Array.from(document.getElementsByClassName("btnEditQuery")).forEach(
+        (btn, index) => btn.addEventListener("click", openEditQuery(index))
+      );
+    }
+  } catch (error) {
+    console.log({ error });
+  }
+};
+
+renderQueries();
